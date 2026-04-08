d4c9a2a8c9ddcf7c01132876bba16c045d09c2bf
Monique Schmidt <Monique_Michelle.schmidt@yahoo.de>
2026-04-08 13:29:09 +0200

    Farbe hi8nzugefügt und Daten

--- kalender.js
+++ kalender.js
@@ -1,3 +1,52 @@
-let jahr = 2026;
-let monat = 3;
-let heute = new Date ();
+let aktuellesJahr = 2026;
+let aktuellerMonat = 3;
+let heute = new Date();
+let heutigerTag = heute.getDate();
+let heutigerMonat = heute.getMonth();
+let heutigesJahr = heute.getFullYear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById=("alterMonat");
+document.getElementById=("neuerMonat");
+
+document.createElement
+
